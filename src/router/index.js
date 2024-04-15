75893d8f1a9436531501187a695b2644c9c8b2c8
Alarma1 <zapara03@yandex.ru>
2024-04-15 19:50:44 +0500

    изменил настройки router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import AuthorizationPage from "../views/AuthorizationPage.vue";
 import AnalyticsPage from "../views/AnalyticsPage.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
     routes: [
         {path: '/', name: 'authorization', component: AuthorizationPage},
         {path: '/analytics', name: 'analytics', component: AnalyticsPage},
